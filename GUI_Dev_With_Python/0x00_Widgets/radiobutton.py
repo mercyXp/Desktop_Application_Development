c686d1a48bbe058b661300ed234062a5c201f414
MercyXp <munzenzimercy2024@gmail.com>
2025-10-22 15:03:09 +0200

    radio button

---
+++ GUI_Dev_With_Python/0x00_Widgets/radiobutton.py
@@ -0,0 +1,14 @@
+import tkinter as tk 
+
+root = tk.Tk()
+root.title("Radio Button")
+root.minsize(300,200)
+
+# tk.Radiobutton(root, text ="Metabrains", value = 1).pack()
+
+for text, value in [("Apple", 1),("Banana",2), ("Grape",3)]:
+    tk.Radiobutton(root, text=text, value=value, indicator = 0).pack()
+
+
+
+root.mainloop()
